8307d9151cbffc999019f30ec7943fbd2d77ed75
00015730 <146339382+Umidjon1717@users.noreply.github.com>
2026-04-16 10:24:32 -0400

    About page updt

--- src/pages/auth/profile/self/Self.tsx
+++ src/pages/auth/profile/self/Self.tsx
@@ -5,23 +5,23 @@ const Self = () => {
   const { data } = useCheckTokenQuery(null);
   return (
     <div className="max-w-4xl mx-auto p-6 bg-white dark:bg-zinc-900 shadow-lg rounded-xl">
-      <h2 className="text-2xl font-semibold mb-4">Mijoz Ma'lumotlari</h2>
+      <h2 className="text-2xl font-semibold mb-4">User Information</h2>
       <div className=" flex flex-col gap-2">
         <p>
-          <strong>Ism:</strong> {data?.customer?.first_name}
+          <strong>Name:</strong> {data?.customer?.first_name}
         </p>
         <p>
-          <strong>Familiya:</strong> {data?.customer?.last_name}
+          <strong>Surname:</strong> {data?.customer?.last_name}
         </p>
         <p>
-          <strong>Telefon:</strong> {data?.customer?.phone_number}
+          <strong>Phone:</strong> {data?.customer?.phone_number}
         </p>
         <p>
           <strong>Email:</strong> {data?.customer?.email}
         </p>
         <div>
           <button className="py-2 mt-3 px-4 bg-bg-primary text-white rounded-md">
-            Malumotlarni o'zgartirish
+            Edit Information
           </button>
         </div>
       </div>
