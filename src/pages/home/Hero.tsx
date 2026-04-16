0ad9407395edb562554dce3cf5210c6b322da005
00015730 <146339382+Umidjon1717@users.noreply.github.com>
2026-04-16 11:45:32 -0400

    compare

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom";
+
 const Hero = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="relative bg-hero-image bg-cover bg-center font-poppins h-[716px] lg:h-[650px] md:h-[500px] sm:h-[400px] max-sm:h-[320px]">
       <div className="absolute inset-0 bg-black opacity-0 dark:opacity-50"></div>
@@ -11,10 +15,10 @@ const Hero = () => {
             Discover Our New Collection
           </h1>
           <p className="text-gray-400 mb-6 text-xl lg:text-lg md:text-base sm:text-sm max-sm:text-xs max-sm:mb-3">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
-            tellus, luctus nec ullamcorper mattis.
+            Elevate your space with thoughtfully crafted furniture—modern
+            comfort, timeless design, and quality you can feel every day.
           </p>
-          <button className="w-[210px] h-[60px] max-sm:text-[9px] lg:w-[180px] lg:h-[50px] md:w-[160px] md:h-[45px] sm:w-[140px] sm:h-[40px] max-sm:w-[100px] max-sm:h-[25px] bg-bg-primary text-white px-6 py-3 max-sm:px-2 max-sm:py-1 font-bold text-base max-sm:text-xs rounded-sm hover:bg-yellow-700 duration-300 max-sm:font-medium">
+          <button onClick={() => navigate("/shop")} className="w-[210px] h-[60px] max-sm:text-[9px] lg:w-[180px] lg:h-[50px] md:w-[160px] md:h-[45px] sm:w-[140px] sm:h-[40px] max-sm:w-[100px] max-sm:h-[25px] bg-bg-primary text-white px-6 py-3 max-sm:px-2 max-sm:py-1 font-bold text-base max-sm:text-xs rounded-sm hover:bg-yellow-700 duration-300 max-sm:font-medium">
             BUY NOW
           </button>
         </div>
